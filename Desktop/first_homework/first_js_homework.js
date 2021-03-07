0e0273f9209b61ec1b8489c8c2ebd36a0a4ddb6c
hayleytaylor29 <hft929@gmail.com>
2021-03-07 13:29:33 -0500

    first section of HW

---
+++ Desktop/first_homework/first_js_homework.js
@@ -0,0 +1,7 @@
+//SECTION 1
+
+//1. DRY - DON'T REPEAT YOURSELF - Repeating code over and over can make it difficult to read and understand. Also can make things more difficult to access in other parts
+//of your program. It is better to have a more streamlined code to understand more easily and to work with again in the future/make changes.
+//2. const - we use this on a variable that will not change, for example <const lastName> - the person's last name will not likely change so we use const
+//   let - we use this when the value of the variable can change - local scope
+//   var - global scope - can be used and accessed anywhere in the code
